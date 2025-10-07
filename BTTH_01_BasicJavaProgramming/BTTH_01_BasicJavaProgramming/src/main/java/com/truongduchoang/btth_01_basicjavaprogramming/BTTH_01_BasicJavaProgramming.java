35ca91bbc51547894141ba966e1fda599940cb27
Truong Duc Hoang <tantiennguyen2404@gmail.com>
2025-10-07 19:21:34 +0700

    hello world with Java

---
+++ BTTH_01_BasicJavaProgramming/BTTH_01_BasicJavaProgramming/src/main/java/com/truongduchoang/btth_01_basicjavaprogramming/BTTH_01_BasicJavaProgramming.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.truongduchoang.btth_01_basicjavaprogramming;
+
+/**
+ *
+ * @author Truong Duc Hoang
+ */
+public class BTTH_01_BasicJavaProgramming {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
